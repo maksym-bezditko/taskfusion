1351386d0b902052a01c2b827e9a5f66234985bb
Max Bezditko <mb.preply.1@gmail.com>
2024-07-08 21:35:32 +0300

    decouple sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,16 @@
+import styles from "./Sidebar.module.scss";
+
+import { MenuNavigation } from "./MenuNavigation";
+import { Logo } from "./svg/Logo";
+
+export const Sidebar = () => {
+  return (
+    <aside className={styles.menu}>
+      <div>
+        <Logo />
+      </div>
+
+      <MenuNavigation />
+    </aside>
+  );
+};
