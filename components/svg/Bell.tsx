4805bb269b7fd0c2eb07c14078e0b1d57cf245ab
Max Bezditko <mb.preply.1@gmail.com>
2024-07-09 12:23:11 +0300

    refactor svg icons

--- components/svg/Bell.tsx
+++ components/svg/Bell.tsx
@@ -13,17 +13,17 @@ export const Bell = (props: Props) => (
     <path
       d="M24.5001 10.8889C24.5001 8.72295 23.6397 6.64573 22.1081 5.11419C20.5766 3.58264 18.4993 2.72223 16.3334 2.72223C14.1675 2.72223 12.0902 3.58264 10.5587 5.11419C9.02711 6.64573 8.16669 8.72295 8.16669 10.8889C8.16669 20.4166 4.08334 23.1389 4.08334 23.1389H28.5834C28.5834 23.1389 24.5001 20.4166 24.5001 10.8889Z"
       stroke="black"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
 
     <path
       d="M18.6882 28.5834C18.4489 28.9959 18.1054 29.3383 17.6921 29.5764C17.2789 29.8144 16.8103 29.9397 16.3334 29.9397C15.8565 29.9397 15.388 29.8144 14.9747 29.5764C14.5615 29.3383 14.218 28.9959 13.9787 28.5834"
       stroke="black"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
 
     {props.isActive && (
--- components/svg/Letter.tsx
+++ components/svg/Letter.tsx
@@ -13,17 +13,17 @@ export const Letter = (props: Props) => (
     <path
       d="M6.66665 6.66666H25.3333C26.6166 6.66666 27.6666 7.71666 27.6666 8.99999V23C27.6666 24.2833 26.6166 25.3333 25.3333 25.3333H6.66665C5.38331 25.3333 4.33331 24.2833 4.33331 23V8.99999C4.33331 7.71666 5.38331 6.66666 6.66665 6.66666Z"
       stroke="black"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
 
     <path
       d="M27.6666 9L16 17.1667L4.33331 9"
       stroke="black"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
 
     {props.isActive && <circle cx="28" cy="7" r="4" fill="#FD633D" />}
