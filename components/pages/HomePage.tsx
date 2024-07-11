d88523c1e8f77a82e94d4d7296ab587449c78f5b
Max Bezditko <mb.preply.1@gmail.com>
2024-07-11 14:32:14 +0300

    add tasks and documents && improve list view layout

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -5,6 +5,8 @@ import { Status } from "../global/Status/Status";
 import { ListView } from "../global/ListView/ListView";
 import { TextWithIcon } from "../global/TextWithIcon/TextWithIcon";
 import { Props as ListItemProps } from "../global/ListItem/ListItem";
+import { Download } from "../svg/Download";
+import { Upload } from "../svg/Upload";
 
 const DETAILS = [
   {
@@ -31,7 +33,7 @@ const DETAIL_STRING =
 const PROJECTS: ListItemProps[] = [
   {
     title: "ОсОО “Energi.kg”",
-    columns: [
+    data: [
       <TextWithIcon key={1} iconName="sunrise" text="12/04/2021" />,
       <TextWithIcon key={2} iconName="sunset" text="12/04/2021" />,
       <TextWithIcon key={3} iconName="people" text="Adyl, Azhar, Arthur" />,
@@ -39,7 +41,7 @@ const PROJECTS: ListItemProps[] = [
   },
   {
     title: "TextLab",
-    columns: [
+    data: [
       <TextWithIcon key={1} iconName="sunrise" text="12/04/2021" />,
       <TextWithIcon key={2} iconName="sunset" text="12/04/2021" />,
       <TextWithIcon key={3} iconName="people" text="Adyl, Azhar, Arthur" />,
@@ -47,7 +49,7 @@ const PROJECTS: ListItemProps[] = [
   },
   {
     title: "ComLab",
-    columns: [
+    data: [
       <TextWithIcon key={1} iconName="sunrise" text="12/04/2021" />,
       <TextWithIcon key={2} iconName="sunset" text="12/04/2021" />,
       <TextWithIcon key={3} iconName="people" text="Adyl, Azhar, Arthur" />,
@@ -58,7 +60,7 @@ const PROJECTS: ListItemProps[] = [
 const PAYMENTS: ListItemProps[] = [
   {
     title: "August Invoice",
-    columns: [
+    data: [
       <TextWithIcon key={1} iconName="sunrise" text="12/04/2021" />,
       <TextWithIcon key={2} iconName="sunset" text="12/04/2021" />,
       <p key={3} className="g_price">
@@ -68,7 +70,7 @@ const PAYMENTS: ListItemProps[] = [
   },
   {
     title: "July Invoice",
-    columns: [
+    data: [
       <TextWithIcon key={1} iconName="sunrise" text="12/04/2021" />,
       <TextWithIcon key={2} iconName="sunset" text="12/04/2021" />,
       <p key={3} className="g_price">
@@ -78,13 +80,52 @@ const PAYMENTS: ListItemProps[] = [
   },
   {
     title: "June Invoice",
-    columns: [
+    data: [
       <TextWithIcon key={1} iconName="sunrise" text="12/04/2021" />,
       <TextWithIcon key={2} iconName="sunset" text="12/04/2021" />,
       <p key={3} className="g_price">
         c 7890
       </p>,
     ],
+  },
+];
+
+const TASKS: ListItemProps[] = [
+  {
+    title: "Action Navigation",
+    data: [<TextWithIcon key={1} iconName="sunset" text="12/04/2021" />],
+    type: "row",
+  },
+  {
+    title: "Notifications",
+    data: [<TextWithIcon key={1} iconName="sunset" text="12/04/2021" />],
+    type: "row",
+  },
+  {
+    title: "Notifications",
+    data: [<TextWithIcon key={1} iconName="sunset" text="12/04/2021" />],
+    type: "row",
+  },
+];
+
+const DOCUMENTS: ListItemProps[] = [
+  {
+    title: "Document_9",
+    data: [<TextWithIcon key={1} iconName="upload" text="12/04/2021" />],
+    type: "row",
+    right: <Download />,
+  },
+  {
+    title: "Invoice_3",
+    data: [<TextWithIcon key={1} iconName="upload" text="12/04/2021" />],
+    type: "row",
+    right: <Download />,
+  },
+  {
+    title: "Assignment_1",
+    data: [<TextWithIcon key={1} iconName="upload" text="12/04/2021" />],
+    type: "row",
+    right: <Download />,
   },
 ];
 
@@ -100,15 +141,27 @@ export const HomePage = () => {
           <Details details={DETAIL_STRING} />
         </div>
 
-        <ListView
-          title="Projects"
-          rightElement={
-            <TextWithIcon iconName="export" text="Export" isClickable />
-          }
-          listItems={PROJECTS}
-        />
+        <div className={styles.listItemsWrapper}>
+          <ListView
+            title="Projects"
+            rightElement={
+              <TextWithIcon iconName="export" text="Export" isClickable />
+            }
+            listItems={PROJECTS}
+          />
 
-        <ListView title="Payments" listItems={PAYMENTS} />
+          <ListView title="Tasks" listItems={TASKS} />
+
+          <ListView title="Payments" listItems={PAYMENTS} />
+
+          <ListView
+            title="Documents"
+            rightElement={
+              <Upload />
+            }
+            listItems={DOCUMENTS}
+          />
+        </div>
       </div>
     </div>
   );
