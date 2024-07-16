88ee59a512ab068b16d62ec76c5b8ce0147bc891
Max Bezditko <mb.preply.1@gmail.com>
2024-07-16 18:43:13 +0300

    change button and side menu

--- components/global/Button/Button.tsx
+++ components/global/Button/Button.tsx
@@ -1,19 +1,41 @@
 import { ReactNode } from "react";
 import styles from "./Button.module.scss";
 import classNames from "classnames";
+import { IoIosArrowDown } from "react-icons/io";
 
 type Props = {
   text: string;
+  isModalButton?: boolean;
   icon?: ReactNode;
-  color?: "orange" | "red" | "green";
+  textColor?: "white" | "black";
+  bgColor?: "orange" | "red" | "green" | "blue" | "gray";
+  width?: string;
+  isFontBold?: boolean;
 };
 
 export const Button = (props: Props) => {
-  const { text, icon, color = "orange" } = props;
+  const {
+    text,
+    isModalButton,
+    icon,
+    width,
+    bgColor = "gray",
+    textColor = "black",
+    isFontBold = true,
+  } = props;
 
   return (
-    <button className={classNames(styles.button, styles[color])}>
+    <button
+      className={classNames(
+        styles.button,
+        styles[bgColor],
+        styles["textColor_" + textColor],
+        isFontBold && styles.fontBold
+      )}
+      style={{ width }}
+    >
       {icon} <span>{text}</span>
+      {isModalButton && <IoIosArrowDown />}
     </button>
   );
 };
