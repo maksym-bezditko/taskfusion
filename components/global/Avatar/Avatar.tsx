99a75000da970be670753133ca9ad05726845191
Max Bezditko <mb.preply.1@gmail.com>
2024-07-17 23:37:40 +0300

    refactor avatar component

--- components/global/Avatar/Avatar.tsx
+++ components/global/Avatar/Avatar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./Avatar.module.scss";
-import DefaultAvatarImage from "@/components/assets/DefaultAvatar.png";
+import DefaultAvatarImage from "@/components/assets/Avatar.png";
 
 type Props = {
   name: string;
@@ -12,7 +12,7 @@ export const Avatar = (props: Props) => {
 
   return (
     <div className={styles.avatarWrapper}>
-      <Image src={image} alt={name} width={40} height={40} />
+      <Image src={image} alt={name} width={40} height={40} className={styles.roundedImage} />
 
 			<p className={styles.name}>{name}</p>
     </div>
