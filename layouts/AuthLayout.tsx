74658135ff54444fcf6ddd58ad3cd3140390ae69
Max Bezditko <mb.preply.1@gmail.com>
2024-08-03 23:30:18 +0300

    refactor layouts

--- layouts/AuthLayout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import styles from './AuthLayout.module.scss';
-
-export const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className={styles.container}>{children}</div>;
-};
