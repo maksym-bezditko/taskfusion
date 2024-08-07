75348f2215f8833a39d0491eb7a091518372e7cc
Max Bezditko <mb.preply.1@gmail.com>
2024-08-07 13:36:20 +0300

    update styles, types and views

---
+++ types/enums.ts
@@ -0,0 +1,12 @@
+export enum UserType {
+  CLIENT = 'Client',
+  DEVELOPER = 'Developer',
+  PM = 'Project Manager',
+}
+
+export enum QueryKeys {
+  USER_PROFILE = 'USER_PROFILE',
+  PROJECTS = 'PROJECTS',
+  PROJECT = 'PROJECT',
+  TASKS = 'TASKS',
+}
