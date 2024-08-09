0c93aa6e7cbef50448b9bbac8583dc628f63928a
Max Bezditko <mb.preply.1@gmail.com>
2024-08-09 17:55:36 +0300

    add loaders to list view and columns

--- components/common/ListView.tsx
+++ components/common/ListView.tsx
@@ -3,16 +3,34 @@ import { ReactNode } from 'react';
 import { ListItem, Props as ListItemProps } from '@/components/common/ListItem';
 
 import styles from './ListView.module.scss';
+import { Loader } from './Loader';
 import { NoData } from './NoData';
 
 type Props = {
   title: string;
   listItems: ListItemProps[];
   rightElement?: ReactNode;
+  isLoading?: boolean;
 };
 
 export const ListView = (props: Props) => {
-  const { title, listItems, rightElement } = props;
+  const { title, listItems, rightElement, isLoading } = props;
+
+  const contentItems = () => {
+    if (isLoading) {
+      return <Loader isSmall />;
+    }
+
+    if (!listItems?.length) {
+      return <NoData />;
+    }
+
+    return (
+      <div className={styles.listItems}>
+        {listItems.length ? listItems.map((listItem) => <ListItem key={listItem.title} {...listItem} />) : <NoData />}
+      </div>
+    );
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -22,9 +40,7 @@ export const ListView = (props: Props) => {
         {rightElement}
       </div>
 
-      <div className={styles.listItems}>
-        {listItems.length ? listItems.map((listItem) => <ListItem key={listItem.title} {...listItem} />) : <NoData />}
-      </div>
+      {contentItems()}
     </div>
   );
 };
